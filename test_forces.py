973ac1d82b031d5417cc95a3549244e89a313fbe
Olga Bavrina <obavrina@apple.com>
2023-07-12 13:41:58 +0100

    some annotation comments

--- test_forces.py
+++ test_forces.py
@@ -9,6 +9,7 @@ from api import forces
 from data import mock_forces
 from util import site_available
 
+
 @pytest.fixture
 def all_forces(data_source):
     if data_source == 'mock':
@@ -31,6 +32,10 @@ def all_forces(data_source):
             f['site'] = f_site
     return res
 
+
+########################################################################
+# Test that all elements of forces list have 'url' key in the response #
+########################################################################
 def test_all_forces_have_site(all_forces):
     errors = []
     for _ in all_forces:
@@ -38,10 +43,13 @@ def test_all_forces_have_site(all_forces):
             errors.append("Police force {}: no website".format(_['id']))
     assert not errors, "errors occured:\n{}".format("\n".join(errors))
 
+########################################################################
+# Test that all department urls are really valid urls                  #
+########################################################################
 def test_all_site_urls_are_valid(all_forces):
     errors = []
     for _ in all_forces:
-        if _['site'] : v = validators.url(_['site'])
+        if _['site']: v = validators.url(_['site'])
         if not v:
             errors.append("Police force {}: website is not valid: {}".format(_['name'], _['site']))
 
@@ -50,17 +58,23 @@ def test_all_site_urls_are_valid(all_forces):
     )
 
 
+########################################################################
+# Test that all department urls are <something>.police.uk<something>   #
+########################################################################
 def test_all_sites_are_in_police_uk_domain(all_forces):
     errors = []
     for _ in all_forces:
         if _['site'] and validators.url(_['site']):
             url_extract = tldextract.extract(_['site'])
             if url_extract.suffix != 'police.uk':
-                errors.append("Police force {}: website is not in \"police.uk\" domain: {}".format(_['name'], _['site']))
+                errors.append(
+                    "Police force {}: website is not in \"police.uk\" domain: {}".format(_['name'], _['site']))
 
     assert not errors, "errors occured:\n{}".format("\n".join(errors))
 
-
+########################################################################
+# Test that all urls refer to existing site in the Internet            #
+########################################################################
 def test_all_sites_are_available(all_forces):
     errors = []
     for _ in all_forces:
@@ -69,7 +83,9 @@ def test_all_sites_are_available(all_forces):
 
     assert not errors, "errors occured:\n{}".format("\n".join(errors))
 
-
+########################################################################
+# Test that all urls sre unique                                        #
+########################################################################
 def test_all_sites_are_unique(all_forces):
     sorted_forces = sorted(all_forces, key=lambda d: d['id'])
     errors = []
